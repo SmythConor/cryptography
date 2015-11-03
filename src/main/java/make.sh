7ac28a25e06f7fe91a9df5899357b45db435b031
Conor Smyth <cnrsmyth@gmail.com>
2015-11-03 22:51:11 +0000

    Removed tests from make file

--- src/main/java/make.sh
+++ src/main/java/make.sh
@@ -1,7 +1,7 @@
 javac *.java -d ../../../bin/
 
-if [ $? -eq 0 ]; then
-	cd ../test
-	javac -cp $CLASSPATH:../../../bin *.java -d ../../../bin/
-	cd -
-fi
+#if [ $? -eq 0 ]; then
+#	cd ../test
+#	javac -cp $CLASSPATH:../../../bin *.java -d ../../../bin/
+#	cd -
+#fi
