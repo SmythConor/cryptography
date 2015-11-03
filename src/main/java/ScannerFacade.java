38ee32b40ecd7990af9f5781d5ea0d5936679e3d
Conor Smyth <cnrsmyth@gmail.com>
2015-11-03 23:46:48 +0000

    added more comments

--- src/main/java/ScannerFacade.java
+++ src/main/java/ScannerFacade.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
+/**
+* @author Conor Smyth <conor.smyth39@mail.dcu.ie>
+* @since 2015-10-23
+* All work is my own
+* Make reading from a file easier
+*/
 class ScannerFacade {
 	Scanner scanner;
 
+	/**
+	* All args constructor
+	* @param file Name of the file as a String
+	*/
 	public ScannerFacade(String file) {
 		try {
 			this.scanner = new Scanner(new File(file));
@@ -14,14 +24,23 @@ class ScannerFacade {
 		}
 	}
 
+	/**
+	* @see java.util.Scanner#hasNext
+	*/
 	public boolean hasNext() {
 		return scanner.hasNext();
 	}
 
+	/**
+	* @see java.util.Scanner#next
+	*/
 	public String next() {
 		return scanner.next();
 	}
 
+	/**
+	* Close the scanner
+	*/
 	public void close() {
 		this.scanner.close();
 	}
