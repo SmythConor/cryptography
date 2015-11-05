f38ddf4cd85297bbc7472c1cc3489e1a4023c550
Conor Smyth <cnrsmyth@gmail.com>
2015-11-05 23:54:22 +0000

    Revamped Generator

--- src/main/java/KeyGenerator.java
+++ src/main/java/KeyGenerator.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
 import java.util.Random;
+import java.security.SecureRandom;
 
 /**
 * @author Conor Smyth <conor.smyth39@mail.dcu.ie>
@@ -13,14 +14,8 @@ class KeyGenerator {
 	 * Generate key with 128 bits
 	 * @return BigInteger with 128 bits
 	 */
-	public static BigInteger generateKey() {
-		BigInteger key = null;
-
-		do {
-			key = generateBits(STD_BITS);
-		} while(key.bitLength() != STD_BITS);
-
-		return key;
+	public static byte[] generateKey() {
+		return generateBits(STD_BITS);
 	}
 
 	/**
@@ -28,17 +23,16 @@ class KeyGenerator {
 	 * @param bits the number of bits to generate
 	 * @return BigInteger salt with the specified number of bits
 	 */
-	public static BigInteger generateKey(int bits) {
-		BigInteger key = null;
-
-		do {
-			key = generateBits(bits);
-		} while(key.bitLength() != bits);
-
-		return key;
+	public static byte[] generateKey(int bits) {
+		return generateBits(bits);
 	}
 
-	private static BigInteger generateBits(int bits) {
-		return new BigInteger(bits, new Random());
+	private static byte[] generateBits(int bits) {
+		SecureRandom secureRandom = new SecureRandom();
+		byte[] bytes = new byte[bits * 8];
+
+		secureRandom.nextBytes(bytes);
+
+		return bytes;
 	}
 }
